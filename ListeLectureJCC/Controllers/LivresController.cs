79add997f8a04983a845ca5e81e05a261386d7af
Amadou Bailo BAH <coladaitp18-bam@ccicampus.fr>
2019-03-28 12:24:17 +0100

    Ajoutez des fichiers projet.

---
+++ ListeLectureJCC/Controllers/LivresController.cs
@@ -0,0 +1,120 @@
+﻿using ListeLectureJCC.Models;
+using System;
+using System.Collections.Generic;
+using System.Data.SqlClient;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace ListeLectureJCC.Controllers
+{
+    public class LivresController : Controller
+    {
+        public ActionResult Accueil()
+        {
+            return View();
+        }
+
+        public ActionResult Detail(int id)
+        {
+            DetailModel model = ChargerDetailDepuisBDD(id);
+            return View(model);
+
+            //if (TryChargerDetailDepuisBDD(id, out DetailModel model))
+            //{
+            //    return View(model);
+            //}
+            //else
+            //{
+            //    return View("Erreur");
+            //}
+        }
+
+        private DetailModel ChargerDetailDepuisBDD(int idLivre)
+        {
+            using (SqlConnection connection = new SqlConnection(@"Server=.\SQLExpress;Database=ListeLecture;Integrated Security=true"))
+            {
+                connection.Open();
+
+                SqlCommand command = new SqlCommand(
+                    "SELECT Titre, Auteur, Note, DateDebutLecture, DateFinLecture FROM Livre WHERE ID = @idLivre", connection);
+                command.Parameters.AddWithValue("@idLivre", idLivre);
+
+                SqlDataReader reader = command.ExecuteReader();
+
+                //On avance sur la première ligne
+                reader.Read();
+
+                string titre = (string)reader["Titre"];
+                string auteur = (string)reader["Auteur"];
+
+                short? note;
+                if (reader.IsDBNull(2))
+                {
+                    note = null;
+                }
+                else
+                {
+                    note = (byte)reader["Note"];
+                }
+
+                DateTime dateDebutLecture = (DateTime)reader["DateDebutLecture"];
+
+                DateTime? dateFinLecture;
+                if (reader.IsDBNull(4))
+                {
+                    dateFinLecture = null;
+                }
+                else
+                {
+                    dateFinLecture = (DateTime)reader["DateFinLecture"];
+                }
+                                
+                DetailModel model = new DetailModel(titre, auteur, dateDebutLecture, dateFinLecture, note);
+                return model;
+            }
+        }
+
+
+
+        private bool TryChargerDetailDepuisBDD(int idLivre, out DetailModel detailModel)
+        {
+            using (SqlConnection connection = new SqlConnection(@"Server=.\SQLExpress;Database=ListeLecture;Integrated Security=true"))
+            {
+                connection.Open();
+
+                SqlCommand command = new SqlCommand(
+                    "SELECT Titre, Auteur, Note, DateDebutLecture, DateFinLecture FROM Livre WHERE ID = @idLivre", connection);
+                command.Parameters.AddWithValue("@idLivre", idLivre);
+
+                SqlDataReader reader = command.ExecuteReader();
+
+                if (reader.Read())
+                {
+                    string titre = (string)reader["Titre"];
+                    string auteur = (string)reader["Auteur"];
+                    short note = (Byte)reader["Note"];
+                    DateTime dateDebutLecture = (DateTime)reader["DateDebutLecture"];
+                    DateTime dateFinLecture = (DateTime)reader["DateFinLecture"];
+                    detailModel = new DetailModel(titre, auteur, dateDebutLecture, dateFinLecture, note);
+                    return true;
+                }
+                else
+                {
+                    detailModel = null;
+                    return false;
+                }
+            }
+        }
+
+        public ActionResult ConfirmationLecture()
+        {
+            return View();
+        }
+
+        public ActionResult ConfirmationNotation()
+        {
+            return View();
+        }
+    }
+}
---
+++ ListeLectureJCC/Models/DetailModel.cs
@@ -0,0 +1,22 @@
+﻿using System;
+
+namespace ListeLectureJCC.Models
+{
+    public class DetailModel
+    {
+        public string Titre { get; private set; }
+        public string Auteur { get; private set; }
+        public DateTime DateDeDebutDeLecture { get; private set; }
+        public DateTime? DateDeFinDeLecture { get; private set; }
+        public short? Note { get; private set; }
+
+        public DetailModel(string titre, string auteur, DateTime dateDeDebutDeLecture, DateTime? dateDeFinDeLecture, short? note)
+        {
+            Titre = titre;
+            Auteur = auteur;
+            DateDeDebutDeLecture = dateDeDebutDeLecture;
+            DateDeFinDeLecture = dateDeFinDeLecture;
+            Note = note;
+        }
+    }
+}
