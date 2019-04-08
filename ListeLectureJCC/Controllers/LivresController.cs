7203a4fe03a04d7420e21c444266cffad6f17a6b
Amadou Bailo BAH <coladaitp18-bam@ccicampus.fr>
2019-04-08 09:31:15 +0200

    changement

--- ListeLectureJCC/Controllers/LivresController.cs
+++ ListeLectureJCC/Controllers/LivresController.cs
@@ -23,52 +23,7 @@ namespace ListeLectureJCC.Controllers
            
         }
 
-       /* private DetailModel ChargerDetailDepuisBDD(int idLivre)
-        {
-            using (SqlConnection connection = new SqlConnection(@"Server=.\SQLExpress;Database=ListeLecture;Integrated Security=true"))
-            {
-                connection.Open();
-
-                SqlCommand command = new SqlCommand(
-                    "SELECT Titre, Auteur, Note, DateDebutLecture, DateFinLecture FROM Livre WHERE ID = @idLivre", connection);
-                command.Parameters.AddWithValue("@idLivre", idLivre);
-
-                SqlDataReader reader = command.ExecuteReader();
-
-                //On avance sur la première ligne
-                reader.Read();
-
-                string titre = (string)reader["Titre"];
-                string auteur = (string)reader["Auteur"];
-
-                short? note;
-                if (reader.IsDBNull(2))
-                {
-                    note = null;
-                }
-                else
-                {
-                    note = (byte)reader["Note"];
-                }
-
-                DateTime dateDebutLecture = (DateTime)reader["DateDebutLecture"];
-
-                DateTime? dateFinLecture;
-                if (reader.IsDBNull(4))
-                {
-                    dateFinLecture = null;
-                }
-                else
-                {
-                    dateFinLecture = (DateTime)reader["DateFinLecture"];
-                }
-                                
-                DetailModel model = new DetailModel(titre, auteur, dateDebutLecture, dateFinLecture, note);
-                return model;
-            }
-        }
-        */
-
+     
 
         private bool TryChargerDetailDepuisBDD(int idLivre, out DetailModel detailModel)
         {
